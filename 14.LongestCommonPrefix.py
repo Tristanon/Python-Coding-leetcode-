cb81efd3f60b9bb3a65730658d1833f6962eee5e
Tristanon <106542253+Tristanon@users.noreply.github.com>
2022-07-16 16:13:55 +0700

    Update 14.LongestCommonPrefix.py

--- 14.LongestCommonPrefix.py
+++ 14.LongestCommonPrefix.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Solution:
 
     def lcp(self, str1, str2):
@@ -13,20 +12,4 @@ class Solution:
         if not strs:
             return ''
         else:
-=======
-class Solution:
-
-    def lcp(self, str1, str2):
-        i = 0
-        while (i < len(str1) and i < len(str2)):
-            if str1[i] == str2[i]:
-                i = i+1
-            else:
-                break
-        return str1[:i]                                                                                                                                                          
-    def longestCommonPrefix(self, strs):
-        if not strs:
-            return ''
-        else:
->>>>>>> 2ea356f2a0fb324ea544d93a3636a794d84960b3
             return reduce(self.lcp,strs)
