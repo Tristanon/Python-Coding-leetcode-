eb0514b03613596146e84edba6cd9b3beb69a456
Tristanon <106542253+Tristanon@users.noreply.github.com>
2022-07-23 15:40:05 +0700

    Update 58.LengthofLastWord.py

--- 58.LengthofLastWord.py
+++ 58.LengthofLastWord.py
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-i=0
-T=0
-j=0
-a=0
-s = "leetcode"
-if len(s) > 1:
-    for j in range(len(s)):
-        if s.count(s[j]) == 1:
-            a = a + 1
-    print(a)        
-    if a > 0:
-        while T == 0:
-            if s.count(s[i]) == 1:
-                T = T + 1
-            else: 
-                i = i +1
-        print(s.index(s[i]))
-    else:
-        print(-1) 
-else:
-    print(0) 
-=======
 class Solution(object):
     def lengthOfLastWord(self, s):
         """
@@ -32,5 +9,4 @@ class Solution(object):
             return 0
         else:
             return len(a[len(a)-1])
-    
->>>>>>> 2ea356f2a0fb324ea544d93a3636a794d84960b3
+
