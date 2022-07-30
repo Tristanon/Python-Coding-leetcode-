7a78f7ad4bde65ca7a3967ebe3d7989577f8e7e2
Tristanon <106542253+Tristanon@users.noreply.github.com>
2022-07-30 02:05:23 -0700

    Update 217.ContainsDuplicate.py

--- 217.ContainsDuplicate.py
+++ 217.ContainsDuplicate.py
@@ -1,7 +1,5 @@
-nums = [1,1,1,3,3,4,3,2,4,2]
-numSet = list(set(nums))
-print(numSet)
-#print(not(len(numSet) == len(nums)))
-i = 0
-for i in range(len(nums)):
-	print(i)
+class Solution(object):
+    def containsDuplicate(self, nums):
+	nums = [1,1,1,3,3,4,3,2,4,2]
+	numSet = list(set(nums))
+	return numSet
