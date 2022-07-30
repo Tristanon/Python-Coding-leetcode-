facb112c69390139473dd55d5933a23d23b4476d
Tristanon <106542253+Tristanon@users.noreply.github.com>
2022-07-30 02:00:50 -0700

    Update 20. ValidParentheses.py

--- 20. ValidParentheses.py
+++ 20. ValidParentheses.py
@@ -1,9 +1,5 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+class Solution:
+    def isValid(self, s: str) -> bool:
         i=0
         j=1
         T=0
