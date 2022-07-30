816dbf5996a9f64eb117cc1dee6aea00fc0e63a4
Tristanon <106542253+Tristanon@users.noreply.github.com>
2022-07-30 01:26:18 -0700

    Update 13.RomantoInteger.py

--- 13.RomantoInteger.py
+++ 13.RomantoInteger.py
@@ -1,10 +1,6 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        vd = { 
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        SymbolnValue = { 
              "I" : 1,
              "V" : 5,
              "X" : 10,
@@ -12,12 +8,13 @@ class Solution(object):
              "C" : 100,
              "D" : 500,
              "M" : 1000,
-             }
+        }
         n=len(s)
-        rt = 0
+        result = 0
         for i in range(n):
-            if  i==n-1 or vd[s[i]] >= vd[s[i+1]] :
-                rt += vd[s[i]]
+            if  i==n-1 or SymbolnValue[s[i]] >= SymbolnValue[s[i+1]] :
+                result += SymbolnValue[s[i]]
             else :
-                rt -= vd[s[i]]
-        return rt
+                result -= SymbolnValue[s[i]]
+        return result
+        
