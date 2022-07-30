e3719201e1842f1303178ccbd2b19fcb7652d85c
Tristanon <106542253+Tristanon@users.noreply.github.com>
2022-07-30 01:53:02 -0700

    Update 151.ReverseWordsinaString.py

--- 151.ReverseWordsinaString.py
+++ 151.ReverseWordsinaString.py
@@ -1,9 +1,5 @@
-class Solution(object):
-    def reverseWords(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
+class Solution:
+    def reverseWords(self, s: str) -> str:
         a = s.split()[::-1]
         i = 0
         result = ''
