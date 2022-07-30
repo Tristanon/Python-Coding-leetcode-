64bbe5a4b2f1abe9c933cbf4b9f762991ea0842a
Tristanon <106542253+Tristanon@users.noreply.github.com>
2022-07-30 01:09:09 -0700

    Update 1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -1,13 +1,7 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        
-        nums: List[int]
-        target: int
-         List[int]
-        
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
-        	for j in range(i + 1, len(nums)):
-        		Sum = nums[i] + nums[j]
-        		if Dum == target:
-        			return[i,j] 
-
+            for j in range(i + 1, len(nums)):
+                Sum = nums[i] + nums[j]
+                if Sum == target:
+                    return[i,j]
