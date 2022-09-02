930c9561c9c77b4382446352be7c447f7909fc9a
Tristanon <106542253+Tristanon@users.noreply.github.com>
2022-09-02 12:34:20 -0600

    Update 136.SingleNumber.py

--- 136.SingleNumber.py
+++ 136.SingleNumber.py
@@ -10,6 +10,7 @@ class Solution:
                 return nums[idx]
             idx +=1
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#second solution
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         r = nums[0]
