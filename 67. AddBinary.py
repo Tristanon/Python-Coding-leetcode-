aa7f1c0ae56dce5957ba8710b2d3a34aa8d604a1
Tri Ho <106542253+Tristanon@users.noreply.github.com>
2022-09-27 21:26:13 -0600

    Create 67. AddBinary.py

---
+++ 67. AddBinary.py
@@ -0,0 +1,29 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        length = min(len(a),len(b))
+        res = ''
+        carry = 0
+        for i in range(1,length+1):
+            if int(a[-i]) + int(b[-i]) + carry > 1:
+                res = str(int(a[-i]) + int(b[-i]) + carry - 2) + res
+                carry = 1
+            else:
+                res = str(int(a[-i]) + int(b[-i]) + carry) + res
+                carry = 0
+        if len(a) > len(b):
+            rest = a[:(len(a)-length)]
+        elif len(a) < len(b):
+            rest = b[:(len(b)-length)]
+        else:
+            rest = ""
+        for i in range(1,len(rest)+1):
+            if int(rest[-i]) + carry > 1:
+                res = str(int(rest[-i]) + carry - 2) + res
+                carry = 1
+            else:
+                res = str(int(rest[-i]) + carry ) + res
+                carry = 0
+        if carry == 0:
+            return res
+        else:
+            return "1"+res
