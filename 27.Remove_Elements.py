073afc8cbcb2864143715d4465dc53fc4c854fd6
Tristanon <razerdv4life@gmail.com>
2023-01-20 10:22:29 -0700

    second solution

--- 27.Remove_Elements.py
+++ 27.Remove_Elements.py
@@ -1,3 +1,4 @@
+# first solution:
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         index = 0
@@ -8,3 +9,21 @@ class Solution:
                 nums.remove(nums[index])
             else:
                 index +=1
+
+# second solution:
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        current_index = 0
+        change_index = 0
+        k = 0
+        if len(nums) == 1 and nums[0] != val:
+            k = 1
+            return k
+        while current_index <= len(nums)-1:
+            if nums[current_index] != val:
+                nums[current_index], nums[change_index] = nums[change_index],nums[current_index]
+                change_index +=1
+                k +=1
+                
+            current_index +=1
+        return k
