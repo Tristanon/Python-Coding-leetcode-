db4bf042aeeea08dd61a5bb15e2f5fd1f0311dab
Tristanon <razerdv4life@gmail.com>
2023-02-16 00:21:36 -0700

    second solution

--- 1004.Max_Consecutive_Ones_III.py
+++ 1004.Max_Consecutive_Ones_III.py
@@ -20,3 +20,13 @@ class Solution:
             max_length = max(max_length,current_length)
             
         return max_length
+# Second attempt:
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        left = 0
+        for right in range(len(nums)):
+            k -= 1 - nums[right]
+            if k < 0:
+                k += 1 - nums[left]
+                left += 1
+        return right - left + 1
--- 2270.Number_of_Ways_to_Split_Array.py
+++ 2270.Number_of_Ways_to_Split_Array.py
@@ -12,3 +12,17 @@ class Solution:
                 answer +=1
 
         return answer
+# Second solution:
+class Solution:
+    def waysToSplitArray(self, nums: List[int]) -> int:
+        prefix_sum = [nums[0]]
+        for index in range(1,len(nums)):
+            prefix_sum.append(nums[index] + prefix_sum[-1])
+
+        answer = 0
+        for index in range(len(nums)-1):
+            left = prefix_sum[index]
+            right = prefix_sum[-1] - prefix_sum[index]
+            if left >= right:
+                answer += 1
+        return answer 
