7f8348fc8080f3e59b56db886e75b953b409138e
Tristanon <razerdv4life@gmail.com>
2023-02-16 17:45:10 -0700

    first solution, very bad which using slice array

---
+++ 557.Reverse_Words_in_a_String_III.py
@@ -0,0 +1,7 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        list_letter = s.split(" ")
+        answer = list_letter[0][::-1]
+        for index in range(1,len(list_letter)):
+            answer += " " + list_letter[index][::-1]
+        return answer
