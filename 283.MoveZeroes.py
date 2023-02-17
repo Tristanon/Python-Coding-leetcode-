56fa9ac540263b044ef61b72f74de16e604b3e63
Tristanon <razerdv4life@gmail.com>
2023-02-17 09:23:34 -0700

    two pointer in MoveZero

--- 283.MoveZeroes.py
+++ 283.MoveZeroes.py
@@ -28,4 +28,18 @@ class Solution:
                 index += 1
         return nums
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-	
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left = right = 0
+        while right < len(nums):
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                right +=1
+                left +=1
+            else:
+                right +=1
+        return nums
+
