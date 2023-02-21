b9ee47ac6b031839532eef6b5e8c12b6a1c5b459
Tristanon <razerdv4life@gmail.com>
2023-02-20 20:42:42 -0700

    second solution but worse

--- 1732.Find_the_Highest_Altitude.py
+++ 1732.Find_the_Highest_Altitude.py
@@ -1,7 +1,17 @@
-# Solution 1
+# Solution 1:
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
         answer = [0]
         for index in range(len(gain)):
             answer.append(gain[index] + answer[-1])
         return max(answer)
+
+# Solution 2:
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        answer = [0]
+        highest_altitude = answer[0]
+        for index in range(len(gain)):
+            answer.append(gain[index] + answer[-1])
+            highest_altitude = max(answer[-1], highest_altitude)
+        return highest_altitude
