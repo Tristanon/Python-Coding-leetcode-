405ba14715bf4d74d9f19a2d5e9faf4bd5eed2b5
Tristanon <razerdv4life@gmail.com>
2023-03-05 13:59:13 -0700

    second approach

--- Counting_Elements.py
+++ Counting_Elements.py
@@ -1,4 +1,4 @@
-# First solution:
+# First approach:
 class Solution:
     def countElements(self, arr: List[int]) -> int:
         hash_set = set(arr)
@@ -7,4 +7,14 @@ class Solution:
             if num + 1 in hash_set:
                 count +=1
         return count
+# Second approach:
+class Solution:
+    def countElements(self, arr: List[int]) -> int:
+        # Do not have to use hashset for this problem
+        count = 0
+        for num in arr:
+            if num + 1 in arr:
+                count +=1
+        return count
+        
         
