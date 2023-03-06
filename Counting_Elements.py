8a091af28c0c46a59cca911f165d31542e48c4ba
Tristanon <razerdv4life@gmail.com>
2023-03-05 17:17:10 -0700

    third solution

--- Counting_Elements.py
+++ Counting_Elements.py
@@ -16,5 +16,18 @@ class Solution:
             if num + 1 in arr:
                 count +=1
         return count
+# Third approach:
+class Solution:
+    def countElements(self, arr: List[int]) -> int:
+        arr.sort()
+        count = 0
+        run_length = 1
+        for i in range(len(arr)):
+            if arr[i - 1] != arr[i]:
+                if arr[i - 1] + 1 == arr[i]:
+                    count += run_length
+                run_length = 0
+            run_length += 1
+        return count
         
         
