473af3dad5beaf8e99cdbf5c2f2cd7a73c2be5e6
Tristanon <razerdv4life@gmail.com>
2023-03-09 15:18:03 -0700

    second solution

--- Maximum_Number_of_Balloons.py
+++ Maximum_Number_of_Balloons.py
@@ -19,3 +19,22 @@ class Solution:
         ans = min(ans,counts['o']//2)
         ans = min(ans,counts['n'])
         return ans
+
+# Solution 2:
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        b_count, a_count, l_count, o_count, n_count = 0, 0, 0, 0, 0
+        for char in text:
+            if char == 'b':
+                b_count+=1
+            elif char == 'a':
+                a_count+=1
+            elif char == 'l':
+                l_count+=1
+            elif char == 'o':
+                o_count+=1
+            elif char =='n':
+                n_count+=1
+        l_count //=2
+        o_count //=2
+        return min(b_count, a_count, l_count, o_count, n_count)
