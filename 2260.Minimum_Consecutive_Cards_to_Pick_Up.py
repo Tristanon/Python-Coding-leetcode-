6c70f6816518e408fa76d0779aa18ef85fb3ce0e
Tristanon <razerdv4life@gmail.com>
2023-03-11 16:30:39 -0700

    second solution

--- 2260.Minimum_Consecutive_Cards_to_Pick_Up.py
+++ 2260.Minimum_Consecutive_Cards_to_Pick_Up.py
@@ -20,5 +20,22 @@ class Solution:
         if answer == float(inf):
             return -1
         return answer
+
+# Second Solution:
+from collections import defaultdict
+
+class Solution:
+    def minimumCardPickup(self, cards: List[int]) -> int:
+        dic = defaultdict(list)
+        for i in range(len(cards)):
+            dic[cards[i]].append(i)
+            
+        ans = float("inf")
+        for key in dic:
+            arr = dic[key]
+            for i in range(len(arr) - 1):
+                ans = min(ans, arr[i + 1] - arr[i] + 1)
+        
+        return ans if ans < float("inf") else -1
             
             
