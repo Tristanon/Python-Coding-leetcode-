8e55d7d31e75587d1cd43906329f72cf6db103c8
Tristanon <razerdv4life@gmail.com>
2023-03-12 17:55:02 -0600

    my own solution

--- Longest_Substring_Without_Repeating_Characters.py
+++ Longest_Substring_Without_Repeating_Characters.py
@@ -12,5 +12,28 @@ class Solution:
             if biggest_range < i - start_index + 1:
                 biggest_range = i - start_index + 1
         return biggest_range
+# My solution;
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if len(s) == 1:
+            return 1
+        answer = current = left = 0
+        seen = set()
+        for right in range(len(s)):
+            if s[right] not in seen:
+                seen.add(s[right])
+                current +=1
+            elif s[right] in seen:
+                answer = max(answer,current)
+                while True:
+                    current -=1
+                    seen.remove(s[left])
+                    if s[left] == s[right]:
+                        seen.add(s[right])
+                        current +=1
+                        left +=1
+                        break
+                    left +=1
+        return max(answer,current)
             
         
