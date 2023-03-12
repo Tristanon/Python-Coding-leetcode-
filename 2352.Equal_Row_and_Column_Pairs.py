58cc54c781c3c7cb339e965921e4f1efa1809e47
Tristanon <razerdv4life@gmail.com>
2023-03-12 15:55:46 -0600

    second solution

--- 2352.Equal_Row_and_Column_Pairs.py
+++ 2352.Equal_Row_and_Column_Pairs.py
@@ -16,3 +16,25 @@ class Solution:
                 if grid[i] == rows[j]:
                     count +=1
         return count
+# Solution 2:
+class Solution:
+    def equalPairs(self, grid: List[List[int]]) -> int:
+        def convert_to_key(array):
+            return tuple(array)
+
+        dic = defaultdict(int)
+        for row in grid:
+            dic[convert_to_key(row)] +=1
+
+        dic2 = defaultdict(int)
+        for index in range(len(grid[0])):
+            current_column = []
+            for row in grid:
+                current_column.append(row[index])
+            dic2[convert_to_key(current_column)] +=1
+        print(dic)
+        print(dic2)
+        answer = 0
+        for array in dic:
+            answer += dic[array] * dic2[array]
+        return answer
