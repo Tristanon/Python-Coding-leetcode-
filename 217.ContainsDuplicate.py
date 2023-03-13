bd0336cbc611d163b947b28642a051a89997e247
Tristanon <razerdv4life@gmail.com>
2023-03-12 18:01:28 -0600

    current practice

--- 217.ContainsDuplicate.py
+++ 217.ContainsDuplicate.py
@@ -1,5 +1,13 @@
+# Last Solution:
 class Solution(object):
     def containsDuplicate(self, nums):
 	nums = [1,1,1,3,3,4,3,2,4,2]
 	numSet = list(set(nums))
 	return numSet
+# Practice again:
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        set_nums = set(nums)
+        if len(nums) == len(set_nums):
+            return False
+        return True
