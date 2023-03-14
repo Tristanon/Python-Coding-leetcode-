b4006ae2092de7558505efca0a5abcd60b72b41e
Tristanon <razerdv4life@gmail.com>
2023-03-14 07:52:11 -0600

    first solution

---
+++ 1496.Path_Crossing.py
@@ -0,0 +1,19 @@
+# 1496.Path_Crossing.py
+# Solution 1:
+class Solution:
+    def isPathCrossing(self, path: str) -> bool:
+        x, y = 0 ,0
+        seen = {(0,0)}
+        for char in path:
+            if char == 'N':
+                y +=1
+            elif char == 'S':
+                y -=1
+            elif char == 'E':
+                x +=1
+            else:
+                x -=1
+            if (x,y) in seen:
+                return True
+            seen.add((x,y))
+        return False
