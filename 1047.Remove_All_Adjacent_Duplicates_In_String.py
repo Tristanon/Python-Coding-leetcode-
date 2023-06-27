86f8dd373d32b608ed25544cca298bc315020f72
Tristanon <razerdv4life@gmail.com>
2023-06-28 01:43:51 +0700

    improve if statement

--- 1047.Remove_All_Adjacent_Duplicates_In_String.py
+++ 1047.Remove_All_Adjacent_Duplicates_In_String.py
@@ -11,4 +11,16 @@ class Solution:
             else:
                 char_stack.pop()
         return "".join(char_stack)
+    
+#Solution 2: using stack to solve the problem and improve the if statements
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        char_stack = []
+        for char in s:
+            if char_stack and char == char_stack[-1]:
+                char_stack.pop()
+            else:
+                char_stack.append(char)
+        return "".join(char_stack)
+         
         
